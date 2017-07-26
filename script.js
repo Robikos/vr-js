ef61fd631c9eb5237c6611581bb81f46d9ee110c
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2017-07-26 03:24:00 +0200

    Fix back camera

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   if (navigator.getUserMedia) {
 
     navigator.mediaDevices.enumerateDevices().then(function(deviceInfos) {
-      deviceId = deviceInfos[deviceInfos.length - 1].deviceId;
-      console.log(deviceId);
+      // deviceId = deviceInfos[deviceInfos.length - 1].deviceId;
+      camera = deviceInfos.find(function(device) { device.kind == "videoinput" });
+      console.log(camera.deviceId);
       navigator.getUserMedia(
         {
           audio: false,
-          video: { deviceId: { exact: deviceId } }
+          video: { deviceId: { exact: camera.deviceId } }
         }, function(stream) {
           video.src = window.URL.createObjectURL(stream);
         }, function(err) { console.log("Error inside"); }
