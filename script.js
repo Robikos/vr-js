bde8358e5c01fdbb54f209749ffd2c48dc764989
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2017-07-26 02:53:03 +0200

    VR JS version 1

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("mousemove", function(event) {
+  canvas = document.getElementsByClassName("canvas")[0];
+  angle = parseInt(event.pageX * (-150 / 1400) + 40);
+  transform_property = "perspective(500px) rotate3d(0, 1, 0, " + angle + "deg) translate3d(0, 0, -150px)";
+  canvas.style.transform = transform_property;
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  navigator.getUserMedia  = navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia;
+
+  video = document.querySelector('video');
+
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({audio: true, video: true}, function(stream) {
+      video.src = window.URL.createObjectURL(stream);
+    }, function(err) { console.log("Error inside"); });
+  } else {
+    console.log("ERROR");
+  }
+});
